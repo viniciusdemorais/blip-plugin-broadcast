f236c0d7a62344293fb1859f2ea578622b3e860f
Daniel Rodrigues da Silva <daniels@take.net>
2020-02-14 15:34:41 -0300

    FIX: Remove sensitive data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   production: true,
   hmr: false,
   version: env.npm_package_version,
-  serverUrl: 'https://takebroadcast.cs.blip.ai/api/v1/',
+  serverUrl: '',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
